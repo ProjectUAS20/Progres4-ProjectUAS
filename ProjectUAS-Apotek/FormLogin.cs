9359e3674ad538c4944862ead2573894e28b674d
Arif Chandra Firmansyah <Sa354127@gmail.com>
2022-05-12 23:38:06 +0700

    menambahkan project 4

---
+++ ProjectUAS-Apotek/FormLogin.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectUAS_Apotek
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void FormLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            User user = new User();
+            user.username = textBoxUsername.Text;
+            user.password = textBoxPassword.Text;
+            if (user.validasi())
+            {
+                MessageBox.Show("User berhasil login");
+                FormMenu frmMenu = new FormMenu();
+                frmMenu.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("User gagal login");
+                textBoxUsername.Text = "";
+                textBoxPassword.Text = "";
+            }
+        }
+    }
+}
